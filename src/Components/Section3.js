145926b95a724b24fd6e955a3f19d28781640fb2
Yeam <yeamlakneheshetu9@gmail.com>
2024-03-19 23:35:20 +0300

    Section 3 adde

---
+++ src/Components/Section3.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Section3() {
+  return (
+    <div>
+<section class="for-all" id="section3">
+        <div class="all-div" id="div3">
+            <div class="line1">
+               iPhone 11
+            </div>
+            <div class="line2">
+                Just the right amount of everything.
+            </div>
+            <div class="line3">
+                From $16.62/mo. or $399 with trade-in.
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#">Learn more</a></li>
+                    <li><a href="#">Buy</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>   
+    </div>
+  );
+}
+
+export default Section3;
