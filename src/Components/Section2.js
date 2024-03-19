46514d9d42254c0dbdcf6e6c4823846a0aced665
Yeam <yeamlakneheshetu9@gmail.com>
2024-03-19 23:33:10 +0300

    other section added

---
+++ src/Components/Section2.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Section2() {
+  return (
+    <div>
+   <section class="for-all" id="section2">
+        <div class="all-div" id="div2">
+            <div class="line1">
+               iPhone 11 Pro
+            </div>
+            <div class="line2">
+                Pro cameras. Pro display. Pro performance.
+            </div>
+            <div class="line3">
+                From $24.95/mo. or $599 with trade-in.
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#">Learn more</a></li>
+                    <li><a href="#">Buy</a></li>
+                </ul>
+            </div>
+        </div>
+    </section> 
+    </div>
+  );
+
+}
+export default Section2;
