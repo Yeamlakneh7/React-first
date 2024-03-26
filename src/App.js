d5a56a9b3844b00aace57b3223c23c29ed1d9a7f
Yeam <yeamlakneheshetu9@gmail.com>
2024-03-27 00:50:37 +0300

    Apple homepage added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
+import Header from "./Components/Header";
+import Section1 from "./Components/Section1";
+import Footer from "./Components/Footer";
+import Section2 from "./Components/Section2";
+import Section3 from "./Components/Section3";
+import Section4 from "./Components/Section4";
+import Section5 from "./Components/Section5";
+import Section6 from "./Components/Section6";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-        <h1>Hello world.</h1>
-      </header>
+       <Header />
+       <Section1/>
+       <Section2/>
+       <Section3/>
+       <Section4/>
+       <Section5/>
+       <Section6/>
+       <Footer/>
+       </header>
     </div>
   );
 }
