897bc023afa165208fff884677115e0bf58500c2
Yeam <yeamlakneheshetu9@gmail.com>
2024-03-27 11:19:23 +0300

    files included in App.js and index.js

--- classComponent/class-based-react/src/App.js
+++ classComponent/class-based-react/src/App.js
@@ -1,22 +1,24 @@
+import React, {Component} from "react";
+import Header from "./Components/Header";
+import Section1 from "./Components/Section1";
+import Section2 from "./Components/Section2";
+import Section3 from "./Components/Section3";
+import Last from "./Components/Last";
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header/>
+        <Section1/>
+        <Section2/>
+        <Section3/>
+        <Last/>
       </header>
     </div>
   );
+}
 }
 
 export default App;
