74ad26948efe333a1e3f0c4a81e994a3c9ee716e
Yeam <yeamlakneheshetu9@gmail.com>
2024-04-08 23:59:26 +0300

    ready to commit

---
+++ react-props/react-props/src/Check.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Check extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Check
